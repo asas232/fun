90e22d9ee66d0287c3d755a893f3a061a126ed79
cj <1003413598@qq.com>
2019-08-01 21:40:13 +0800

    修改

--- src/main/java/com/jackc/fun/init/SeleniumInit.java
+++ src/main/java/com/jackc/fun/init/SeleniumInit.java
@@ -1,7 +1,9 @@
 package com.jackc.fun.init;
 
+import lombok.extern.slf4j.Slf4j;
 import net.lightbody.bmp.client.ClientUtil;
 import net.lightbody.bmp.proxy.CaptureType;
+import org.apache.commons.exec.environment.EnvironmentUtils;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
@@ -17,11 +20,12 @@ import java.net.UnknownHostException;
  * @Author chenjie
  * @DATE 2019/7/25 16:31
  */
+@Slf4j
 public class SeleniumInit {
 
     public static WebDriver driver = null;
 
-    public static  void  proxyDriver(){
+    public static void proxyDriver() {
         Proxy seleniumProxy = ClientUtil.createSeleniumProxy(ProxyServer.proxy);
         try {
             String hostIp = Inet4Address.getLocalHost().getHostAddress();
@@ -41,20 +45,28 @@ public class SeleniumInit {
         ProxyServer.proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
     }
 
-    public static void noproxyDriver(){
-        if(InitSeting.isWindow){
+    public static void noproxyDriver() {
+        if (InitSeting.isWindow) {
             System.setProperty("webdriver.chrome.driver", "D:\\devsoft\\chromedriver_win32\\chromedriver.exe");
-        }else {
-            System.setProperty("webdriver.chrome.driver", "/app/.apt/usr/bin/google-chrome");
+//        }else {
+//            System.setProperty("webdriver.chrome.driver", "/app/.apt/usr/bin/google-chrome");
+//        }
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            if (!InitSeting.isWindow) {
+                String binaryPath = null;
+                try {
+                    binaryPath = EnvironmentUtils.getProcEnvironment().get("GOOGLE_CHROME_SHIM");
+                    log.info("binaryPath" + binaryPath);
+                } catch (IOException e) {
+
+                }
+                chromeOptions.setBinary(binaryPath);
+            }
+            if (InitSeting.isWindow) {
+                chromeOptions.addArguments("--proxy-server=socks5://" + "127.0.0.1:1080");
+            }
+            driver = new ChromeDriver(chromeOptions);
         }
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
-        if(!InitSeting.isWindow){
-            chromeOptions.setBinary("/app/.apt/usr/bin/google-chrome");
-        }
-        if(InitSeting.isWindow) {
-            chromeOptions.addArguments("--proxy-server=socks5://" + "127.0.0.1:1080");
-        }
-        driver = new ChromeDriver(chromeOptions);
     }
 }
